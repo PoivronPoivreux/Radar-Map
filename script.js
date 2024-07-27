af1d1a4a43f77a8e93a8e4d670d2add251939441
PoivronPoivreux <100969394+PoivronPoivreux@users.noreply.github.com>
2024-07-28 00:44:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const map = L.map('map').setView([46.603354, 1.888334], 6);
+    const map = L.map('map').setView([46.603354, 1.888334], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
